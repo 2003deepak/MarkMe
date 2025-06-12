bfd3f2de7476a9daf3fa07f6b4c0686da42cad43
2003deepak <deepakkumar74491234@gmail.com>
2025-06-12 21:30:17 +0530

    Imporvemnet in folder structure

--- app/api/v1/routes/student_router.py
+++ app/api/v1/routes/student_router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 router = APIRouter()
 
 
-# Ask first name, last name, email, mobile number, 6 digit pin , dob , roll number, program, department, semester, batch year , 3-4 photos 
+# Ask first name, middle name ( optional ) , last name, email, mobile number, 6 digit pin , dob , roll number, program, department, semester, batch year , 3-4 photos 
 # Now construct unique student ID using Program, Department, Batch Year, Semester, Roll Number
 # Ex : BTECH-CSE-2023-1-12345
 # Convert the students photo in vector embeddings of 512 array size 
